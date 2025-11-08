7e08822ad16a00be16b1a149b9cba0f4f4daa3f5
Sushabhan <sushabhanmajumder878@gmail.com>
2025-11-08 17:22:54 +0530

    Added all the updated speakers

--- data/indianSpeakers.ts
+++ data/indianSpeakers.ts
@@ -69,8 +69,137 @@ const indianSpeakers: IndianSpeaker[] = [
         name: "Dr. Dwaipayan De",
         affiliation: "Department of Mechanical Engineering, Institute of Engineering and Management (IEM) and School of University of Engineering and Management (UEM), Kolkata, India",
         image: "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
-    }
-      
+    },
+        {
+          "name": "Dr. Gautam Anand",
+          "affiliation": "School of Materials Science and Technology, IIT BHU, Varanasi, UP",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Ramesh Singh",
+          "affiliation": "Department of Mechanical Engineering, IIT Bombay",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Suvradip Mullick",
+          "affiliation": "School of Mechanical Sciences, IIT Bhubaneswar, Odisha",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Varun Sharma",
+          "affiliation": "Department of Mechanical and Industrial Engineering, IIT Roorkee",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Manab Mallik",
+          "affiliation": "Department of Metallurgical and Materials Engineering, NIT Durgapur",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Manjaiah M.",
+          "affiliation": "Department of Mechanical Engineering, NIT Warangal",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Supriyo Ghosh",
+          "affiliation": "Department of Metallurgical and Materials Engineering, IIT Roorkee",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Sukla Mondol",
+          "affiliation": "Department of Metallurgical and Materials Engineering, NIT Warangal",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Soham Mujumdar",
+          "affiliation": "Department of Mechanical Engineering, IIT Bombay",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Suryakumar S",
+          "affiliation": "Department of Mechanical Engineering, IIT Hyderabad",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Amit Arora",
+          "affiliation": "Department of Metallurgical Engineering, IIT Gandhinagar",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Anup Kumar Keshri",
+          "affiliation": "Department of Metallurgical and Materials Engineering, IIT Patna",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Mohd Zaheer Khan Yusufzai",
+          "affiliation": "Department of Mechanical Engineering, IIT BHU",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Anirban Bhattacharya",
+          "affiliation": "School of Mechanical Sciences, IIT Bhubaneswar",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Ajit Behera",
+          "affiliation": "Department of Metallurgical and Materials Engineering, NIT Rourkela",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Sagar Sarkar",
+          "affiliation": "Department of Mechanical Engineering, IIT Delhi",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Anirban Naskar",
+          "affiliation": "Department of Mechanical and Aerospace Engineering, IIT Hyderabad",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Vivek Kumar Sahu",
+          "affiliation": "Department of Fuel, Minerals and Metallurgical Engineering, IIT ISM Dhanbad",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Ravi K.R.",
+          "affiliation": "Department of Metallurgical and Materials Engineering, IIT Jodhpur",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Santosh S. Hosmani",
+          "affiliation": "Department of Mechanical Engineering, IIT Indore",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Prof. Kasturi Sala",
+          "affiliation": "Department of Fuel, Minerals and Metallurgical Engineering, IIT ISM Dhanbad",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Manas Kumar Mondal",
+          "affiliation": "Department of Metallurgical and Materials Engineering, NIT Durgapur",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Sudipta Pramanik",
+          "affiliation": "School of Minerals, Metallurgical and Materials Engineering, IIT Bhubaneswar",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Arvind Kumar",
+          "affiliation": "Department of Mechanical Engineering, IIT Kanpur",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Ram Krishna",
+          "affiliation": "Department of Metallurgical and Materials Engineering, NIT Jamshedpur",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        },
+        {
+          "name": "Dr. Ajit Panigrahi",
+          "affiliation": "CSIR-IMMT, Bhubaneswar",
+          "image": "https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg"
+        }
 ]
   
   
