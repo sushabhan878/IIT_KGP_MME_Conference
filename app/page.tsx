e287fd8cd9f030bd2a4024925362356a46d18afb
Sushabhan <sushabhanmajumder878@gmail.com>
2025-12-01 23:22:38 +0530

    abstruct submission deadline updated

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Home = () => {
           <div className="relative overflow-hidden marquee-viewport">
             <div className="animate-marquee whitespace-nowrap py-3 text-amber-900" style={{ animationDuration: '30s' }}>
               <span className="px-10 font-semibold tracking-wider uppercase">
-                Last date of abstract submission is extended till 30th Nov!
+                Last date of abstract submission is extended till 15th December!
               </span>
               <span className="px-6 text-amber-900/60">•</span>
               <span className="px-10 font-medium uppercase">
