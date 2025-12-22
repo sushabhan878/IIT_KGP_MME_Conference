02757dcaa2edb5c8bfd2dac2b9aa8ee431e37555
Sushabhan <sushabhanmajumder878@gmail.com>
2025-12-22 12:03:24 +0530

    added final speakers

--- data/foreignSpeakers.ts
+++ data/foreignSpeakers.ts
@@ -6,40 +6,63 @@ export interface ForeignSpeaker {
 
 const foreignSpeakers: ForeignSpeaker[] = [
     {
-      name: 'Dr.-Ing. Andreas Wetzig',
+      name: 'Prof. Andreas Wetzig,',
       affiliation: 'Technology Field Manager, Cutting & Joining, Fraunhofer IWS, Dresden, Germany',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
-    },
-    {
-      name: 'Prof. Prakash Srirangam',
-      affiliation: 'Associate Professor, University of Warwick, UK',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
-    },
-    {
-      name: 'Prof. Boaz Pokroy',
-      affiliation: 'Department of Materials Science & Engineering, Technion, Israel',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
-    },
-    {
-      name: 'Prof. Narendra B. Dahotre',
-      affiliation: 'Regents Professor, University of North Texas, USA; Associate Vice President, Center for Agile & Adaptive Additive Manufacturing',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
+      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381091/Screenshot_2025-12-22_105305_ddqjdv.png'
   },
-    {
-      name: 'Prof. Chinnasamy Vivekanandhan',
-      affiliation: 'Department of Mechanical, Energy and Industrial Engineering, School of Electrical and Mechanical Engineering, Botswana International University of Science and Technology',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
+  {
+    name: 'Prof. Narendra B. Dahotre',
+    affiliation: 'Regents Professor, University of North Texas, USA; Associate Vice President, Center for Agile & Adaptive Additive Manufacturing',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381093/Screenshot_2025-12-22_105348_gpsdo3.png'
   },
-    {
-      name: 'Dr. Vladimir Popov ',
-      affiliation: 'Tel Aviv University',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
+  {
+    name: 'Prof. Juergen Eckert',
+    affiliation: 'Erich Schmid Institute of Materials Science, Univ. of Leoben, Austria',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766383594/Screenshot_2025-12-22_113618_gimo8z.png'
   },
+  {
+    name: 'Prof. Bekir S. Yilbas',
+    affiliation: 'King Fahd University of Petroleum and Minerals (KFUPM), Saudi Arabia',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381093/Screenshot_2025-12-22_105357_knh0i3.png'
+  },
+  {
+    name: 'Prof. Dr. Frank Brückner',
+    affiliation: 'Technology Field Manager Additive Manufacturing and Surface Technology Fraunhofer Institute for Material and Beam Technology IWS',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381093/Screenshot_2025-12-22_105424_qa9jsy.png'
+  },
+  {
+    name: 'Prof. Kazuhiro Ogawa',
+    affiliation: 'Tohoku University',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381092/Screenshot_2025-12-22_105340_gktzyl.png'
+  },
+  {
+    name: 'Dr. Vladimir Popov ',
+    affiliation: 'Tel Aviv University',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381093/Screenshot_2025-12-22_105407_snkgtb.png'
+},
     {
-      name: 'Prof. Dr. Frank Brückner',
-      affiliation: 'Technology Field Manager Additive Manufacturing and Surface Technology Fraunhofer Institute for Material and Beam Technology IWS',
-      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
-  }
+      name: 'Prof. Stephan Dietrich',
+      affiliation: 'Karlsruhe Institute of Technology, Germany',
+      image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381846/Screenshot_2025-12-22_110550_lvpoq3.png'
+  },
+  {
+    name: 'Prof. Prakash Srirangam',
+    affiliation: 'Associate Professor, University of Warwick, UK',
+    image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381092/Screenshot_2025-12-22_105316_abfsdx.png'
+  },
+  //   {
+  //     name: 'Prof. Boaz Pokroy',
+  //     affiliation: 'Department of Materials Science & Engineering, Technion, Israel',
+  //     image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1766381092/Screenshot_2025-12-22_105329_gzrdo0.png'
+  //   },
+    
+  //   {
+  //     name: 'Prof. Chinnasamy Vivekanandhan',
+  //     affiliation: 'Department of Mechanical, Energy and Industrial Engineering, School of Electrical and Mechanical Engineering, Botswana International University of Science and Technology',
+  //     image: 'https://res.cloudinary.com/damnmi4ya/image/upload/v1756465899/Profile-avatar-Graphics-48866790-1_plqpgn.jpg'
+  // },
+    
+    
   ]  
   
 
